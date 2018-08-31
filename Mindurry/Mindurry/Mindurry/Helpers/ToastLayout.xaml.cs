d28a2f8184a4e97b7cdc4e8aae1f6e73e691da1a
Vaibhav Khanna <y2kvaibhav@gmail.com>
2018-08-31 17:59:26 +0530

    Sync popup added

---
+++ Mindurry/Mindurry/Mindurry/Helpers/ToastLayout.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace Mindurry.Helpers
+{
+    public partial class ToastLayout : PopupPage
+    {
+
+
+        public ToastLayout(string text, Color? color)
+        {
+            InitializeComponent();
+            ToastLabel.Text = text;
+
+            if (color != null)
+            {
+                container.BackgroundColor = (Color)color;
+            }
+        }
+
+        protected override void OnAppearingAnimationEnd()
+        {
+            Device.StartTimer(new TimeSpan(0, 0, 2), () =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Rg.Plugins.Popup.Services.PopupNavigation.Instance.PopAllAsync();
+                });
+
+                return false;
+            });
+
+            base.OnAppearingAnimationEnd();
+        }
+
+    }
+}
---
+++ Mindurry/Mindurry/Mindurry/Helpers/ToastService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mindurry.Models;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace Mindurry.Helpers
+{
+    public static class ToastService
+    {
+
+        public static async Task ShowSyncing(string text)
+        {
+            var popupPage = new SyncPopUp(text);
+
+            if (PopupNavigation.Instance.PopupStack.Count > 0)
+            {
+                await PopupNavigation.Instance.PopAllAsync();
+            }
+
+            await PopupNavigation.Instance.PushAsync(popupPage);
+        }
+
+        public static async Task Show(string text)
+        {
+            var popupPage = new ToastLayout(text, null);
+
+            if (PopupNavigation.Instance.PopupStack.Count > 0)
+            {
+                await PopupNavigation.Instance.PopAllAsync();
+            }
+
+            await PopupNavigation.Instance.PushAsync(popupPage);
+        }
+
+        public static async Task Show(string text, Color ToastColor)
+        {
+            var popupPage = new ToastLayout(text, ToastColor);
+
+            if (PopupNavigation.Instance.PopupStack.Count > 0)
+            {
+                await PopupNavigation.Instance.PopAllAsync();
+            }
+
+            await PopupNavigation.Instance.PushAsync(popupPage);
+        }
+
+        public static async Task Hide()
+        {
+            if (PopupNavigation.Instance.PopupStack.Count > 0)
+            {
+                await PopupNavigation.Instance.PopAllAsync();
+            }
+        }
+
+    }
+}
